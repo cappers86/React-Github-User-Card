4c97f38402dd4e5d59ac5e9ff0ffc3745b9bb431
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-12-03 11:32:07 +0000

    retrieved information from Api Creating user card

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state={
+    name: [],
+    }
+
+    componentDidMount () {
+      axios
+      .get(`https://api.github.com/users/cappers86`)
+      .then (res => {
+        console.log(res.data)
+        this.setState({
+          
+        });
+      })
+      .catch (error => console.log (error));
+    }
+
+    render () {
+      return (
+        <div>
+          <h1>User card</h1>
+        </div>
+      )
+    }
+
 }
+
+
 
 export default App;
